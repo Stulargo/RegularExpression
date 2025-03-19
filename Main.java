cf0d912abca3dc7abd4f1919f4a796b5003e80f8
Stulargo <114095328+Stulargo@users.noreply.github.com>
2025-03-19 08:25:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String tekst = "abc123";
         String tekst2 = "7891234560";
+        //String tekst3 ="xd";
 
         System.out.println(tekst.matches("^[a-zA-Z0-9]{6}$"));
         //System.out.println("a1".matches("^[a-zA-Z0-9]{6}$"));
 
         System.out.println(tekst2.matches("[789]\\d{9}"));
 
+        //System.out.println(tekst3.matches("^[XxDd207]{8}$"));
 
-       
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj haslo");
+
+        String haslo = scanner.nextLine();
+
+
+        if (haslo.matches("^[XxDd]{8}$")) {
+            System.out.println("Haslo jest poprawne");
+        } else {
+            System.out.println("Haslo jest niepoprawne ");
+        }
+
     }
 }
